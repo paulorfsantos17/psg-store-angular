884a55bc9f05fb7e7f17805a2b7267ee2e1a6479
Paulo Santos <paulorfsantos17@gmail.com>
2025-03-31 10:44:13 -0300

    feat: init project

---
+++ src/app/components/title-bar/titla-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titla-bar',
+  imports: [],
+  templateUrl: './titla-bar.component.html',
+  styleUrl: './titla-bar.component.css'
+})
+export class TitlaBarComponent {
+
+}
