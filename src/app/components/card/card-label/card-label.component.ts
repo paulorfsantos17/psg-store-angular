690fca679fba35656629ad88d69662eef1aa778f
Paulo Santos <paulorfsantos17@gmail.com>
2025-03-31 15:04:38 -0300

    feat:add card

---
+++ src/app/components/card/card-label/card-label.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-label',
+  imports: [],
+  templateUrl: './card-label.component.html',
+  styleUrl: './card-label.component.css'
+})
+export class CardLabelComponent {
+
+}
---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  imports: [],
+  templateUrl: './card-pricing.component.html',
+  styleUrl: './card-pricing.component.css'
+})
+export class CardPricingComponent {
+
+}
